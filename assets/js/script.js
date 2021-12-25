62053a750dc4c70081c8259418753be93741761a
SpawN3222 <sinchuk108@gmail.com>
2021-12-26 01:05:08 +0300

    copy_footer_texts

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function copy(text) {
+    let input = document.createElement('textarea');
+    input.innerHTML = text;
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand('copy');
+    document.body.removeChild(input);
+}
