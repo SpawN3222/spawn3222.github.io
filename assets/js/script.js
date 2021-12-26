9dd40ca1b0fddb2a296baa75d08cc37a28ae5a20
SpawN3222 <sinchuk108@gmail.com>
2021-12-26 21:23:51 +0300

    add-discord_clipboard-js_broken_js_script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//! Copying text to clipboard
 function copy(text) {
     let input = document.createElement('textarea');
     input.innerHTML = text;
@@ -6,3 +7,34 @@ function copy(text) {
     document.execCommand('copy');
     document.body.removeChild(input);
 }
+
+
+function setTooltip(e,message) {
+  $(e.trigger).tooltip({
+  trigger: 'click',
+  placement: 'top'
+});
+$(e.trigger).tooltip('hide')
+ .attr('data-original-title', message)
+ .tooltip('show');
+}
+
+function hideTooltip(e) {
+  setTimeout(function() {
+    $(e.trigger).tooltip('hide');
+  }, 1000);
+}
+
+// Clipboard
+
+var clipboard = new Clipboard('footer_element');
+
+clipboard.on('success', function(e) {
+  setTooltip(e,'Copied!');
+  hideTooltip(e);
+});
+
+clipboard.on('error', function(e) {
+  setTooltip(e,'Failed!');
+  hideTooltip(e);
+});
