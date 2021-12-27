ee2771bfdd1a0005643d7d7d82b6f5034bf86b5e
SGezha <sasha-gezha1@mail.ru>
2021-12-27 21:28:08 +0300

    tooltips-copy_script_tweak-resolution_fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,17 @@
 //! Copying text to clipboard
-function copy(text) {
+function copy(text, element) {
     let input = document.createElement('textarea');
     input.innerHTML = text;
     document.body.appendChild(input);
     input.select();
     document.execCommand('copy');
     document.body.removeChild(input);
+
+    if(!element || element.classList.value.includes('active')) return;
+    
+    element.classList.toggle('active');
+    
+    setTimeout(() => {
+      element.classList.toggle('active');
+    }, 2000)
 }
-
-
-function setTooltip(e,message) {
-  $(e.trigger).tooltip({
-  trigger: 'click',
-  placement: 'top'
-});
-$(e.trigger).tooltip('hide')
- .attr('data-original-title', message)
- .tooltip('show');
-}
-
-function hideTooltip(e) {
-  setTimeout(function() {
-    $(e.trigger).tooltip('hide');
-  }, 1000);
-}
-
-// Clipboard
-
-var clipboard = new Clipboard('footer_element');
-
-clipboard.on('success', function(e) {
-  setTooltip(e,'Copied!');
-  hideTooltip(e);
-});
-
-clipboard.on('error', function(e) {
-  setTooltip(e,'Failed!');
-  hideTooltip(e);
-});
